443fd0cc6ac8a63b1da98557e9c23eaa5f7defe2
Parijat Mukherjee <parijatmukherjee11@gmail.com>
2024-09-02 22:22:01 +0530

    Updated problem_parser.py using regex

--- problem_parser.py
+++ problem_parser.py
@@ -52,16 +52,13 @@ text_numbers = {"one": 1,
 # Function to check a line of input for the presence of an array
 def check_array(line):
     if re.search("_", line) is None:
-        print(1)
         return
 
-    match = re.search("[^ ] integers|numbers [^ ]", line)
+    match = re.search("[^ ]+ (integers|numbers) [^ ]+ ", line)
     if match is None:
-        print(2)
         return
 
     var_details = match.group().split()
-    print(var_details)
     qty = var_details[0].strip('$')
     try:
         qty = int(qty)
@@ -70,15 +67,9 @@ def check_array(line):
             qty = text_numbers[qty]
         except KeyError:
             pass
-    name = var_details[-1].strip(',$')
+    name = var_details[2].strip(',$')
 
-    name = re.search(".*_", name).group()[:-1]
+    match = re.search(".*_", name)
+    if match is not None:
+        name = match.group()[:-1]
     return Variable("int[{}]".format(qty), name)
-
-
-s = [#'''The first line contains a single integer $t$ ($1$ $\\le$ $10^4$) - the number of test cases.''',
-'''The only line of each test case contains $n$ integers $a_1$, $a_2$, $...$, $a_n$ ($1$ $\\le$ $l$ $\\le$ $r$ $\\le$ $10^9$).''']
-for x in s:
-    v = check_array(x)
-    if v is not None:
-        print(v.datatype, v.name)

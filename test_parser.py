d62901df9c7e227e1f76928d7c1d9e7540f440bc
Kashyap <kashyap@gmail.con>
2024-09-02 22:38:40 +0530

    Added the pytests

--- test_parser.py
+++ test_parser.py
@@ -16,3 +16,31 @@ def test_scraper():
 #         check_all()
 #     except:
 #         pytest.fail("Parser failed for pair of integers")
+
+# def test_parser_array():
+#     p = Problem("2008", "F")
+#     try:
+#         check_all()
+#     except:
+#         pytest.fail("Parser failed for array of integers")
+
+# def test_parser_string():
+#     p = Problem("1922", "A")
+#     try:
+#         check_all()
+#     except:
+#         pytest.fail("Parser failed for string")
+
+# def test_parser_matrix():
+#     p = Problem("1980", "E")
+#     try:
+#         check_all()
+#     except:
+#         pytest.fail("Parser failed for matrix")
+
+# def test_parser_string_array():
+#     p = Problem("1980", "E")
+#     try:
+#         check_all()
+#     except:
+#         pytest.fail("Parser failed for array of strings")

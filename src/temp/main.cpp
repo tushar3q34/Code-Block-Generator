36220dc9195b6ee4fc26e123e1eca687ca9f2170
Parijat Mukherjee <parijatmukherjee11@gmail.com>
2024-09-05 14:31:17 +0530

    More tests

--- src/temp/main.cpp
+++ src/temp/main.cpp
@@ -3,11 +3,21 @@
 using namespace std;
 
 void solve(){
-    int l;
-    cin >> l;
-    int r;
-    cin >> r;
-
+    int n;
+    cin >> n;
+    int separated[][n];
+    for (int i = 0; i < ; i++){
+        for (int j = 0; j < n; j++){
+            cin >> separated[i][j];
+        }
+    }
+    int separated[n][n-1];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n-1; j++){
+            cin >> separated[i][j];
+        }
+    }
+    
 }
 
 int main(){
--- src/temp/main.py
+++ src/temp/main.py
@@ -10,9 +10,16 @@ def cin():
 
 
 def solve():
-    l = int(cin())
-    r = int(cin())
-
+    n = int(cin())
+    separated = [[None for j in range(n)] for i in range()] 
+    for i in range():
+        for j in range(n):
+            separated[i][j] = int(cin())
+    separated = [[None for j in range(n-1)] for i in range(n)] 
+    for i in range(n):
+        for j in range(n-1):
+            separated[i][j] = int(cin())
+    
 
 
 for tc in range(int(input())):
